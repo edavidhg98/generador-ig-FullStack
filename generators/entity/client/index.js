e40f53e05133c1f293a8348fee97322ea51a9270
SMorales <smorales@intergrupo.com>
2017-08-29 15:49:39 -0500

    Posicionamiento de los controles (input's) del component Upsert

--- generators/entity/client/index.js
+++ generators/entity/client/index.js
@@ -30,11 +30,48 @@ module.exports = class extends Generator {
       this.fs.copyTpl(
         this.templatePath(`angular/entity/_entity${layerName}`),
         this.destinationPath(`src/app/entities/${entityName}/${entityName}${layerName}`),
-        { entityName: this.entityNameFormats,
+        {
+          entityName: this.entityNameFormats,
           attributes: this.entity.attributes,
+          attributesGroupByPosition: this._getAttributesGroupByPosition(this.entity.attributes),
+          attributesWithoutPosition: this._getAttributesWithoutPosition(this.entity.attributes),
           _: _
         }
       );
     });
+  }
+
+  _getAttributesGroupByPosition(attributes) {
+    // Arreglo de attributos ordenados por posición.
+    const attributesSortedByPosition = this._sortAttributesByPosition(attributes);
+
+    // Objeto JSON con los atributos agrupados por filas.
+    const attributesGroupByPosition =  _.groupBy(attributesSortedByPosition, 'position.row');
+
+    // Quitar los atributos sin posicion, y convertir el JSON a un array
+    let attributesArrayGroupByPosition = [];
+    for (let key in attributesGroupByPosition) {
+      if (key !== 'undefined') {
+        attributesArrayGroupByPosition.push(attributesGroupByPosition[key]);
+      }
+    }
+    // Arreglo de attributos agrupados por posiciones.
+    return attributesArrayGroupByPosition;
+  }
+
+  _getAttributesWithoutPosition(attributes) {
+    return attributes.filter(attribute => attribute.position === undefined);
+  }
+
+  _sortAttributesByPosition(attributes) {
+    return attributes.sort((a, b) => {
+      if (a.position === undefined || (a.position.row === undefined || a.position.col === undefined)) {
+         return -1;
+      }
+      if (b.position === undefined || (b.position.row === undefined || b.position.col === undefined)) {
+         return 1;
+      }
+      return (a.position.row - b.position.row) || (a.position.col - b.position.col);
+   });
   }
 }
