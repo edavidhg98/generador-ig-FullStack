db9baf93bdd8d02f613c3739d10cde92169223df
SMorales <yeyox_114@hotmail.com>
2017-09-15 17:06:19 -0500

    Creacion de entidades relacionales en la definicion de los modelos en Mongoose, arreglo del config.json de Sanitas

--- generators/entity/server/templates/api/entidad/_entidad.model.js
+++ generators/entity/server/templates/api/entidad/_entidad.model.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const <%= entityName.camel %>Schema = new Schema({<%attributes.forEach((attribute, index) => { %>
   <%= attribute.name %>: { type: <%= attribute.type %><%- include _entidad-validations -%> }<%_ if (index < attributes.length - 1 || relationships) { _%>,<%_ } _%>
   <% }); %>
+  <%- include _entidad-relationships _%>
+}, { toJSON: { virtuals: true } });
+
+
+<%_ for(let oneToManyRelationShip of oneToManyRelationShips) { _%>
+<%= entityName.camel %>Schema.virtual('<%= oneToManyRelationShip.fieldName %>', {
+  ref: '<%= oneToManyRelationShip.entityRef %>',
+  localField: '<%= oneToManyRelationShip.localField %>',
+  foreignField: '<%= oneToManyRelationShip.foreignField %>'
 });
+<%_ } _%>
 
 module.exports = mongoose.model('<%= entityName.pascal %>', <%= entityName.camel %>Schema);
